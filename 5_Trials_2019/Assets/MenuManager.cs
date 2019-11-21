6496e6f1a15caee31301360db0b3eea9e9f396ca
Cameron <mack679@hotmail.com.au>
2019-11-22 05:18:48 +1100

    Began working on the main menu

---
+++ 5_Trials_2019/Assets/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public List<GameObject> menus = new List<GameObject>();
+
+    private GameObject currentMenu;
+
+    
+    void Start()
+    {
+        //The first menu in the list is loaded first
+        currentMenu = menus[0];
+        currentMenu.SetActive(true);
+
+        for (int i = 1; i < menus.Capacity; i++)
+            menus[i].SetActive(false);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        SceneManager.LoadScene("Rooms");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeMenu(int menu)
+    {
+        currentMenu.SetActive(false);
+        currentMenu = menus[menu];
+        currentMenu.SetActive(true);
+    }
+}
