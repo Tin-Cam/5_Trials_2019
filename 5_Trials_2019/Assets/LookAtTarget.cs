54de29ee538d0fadd86cde6d69f3a72d0c506f15
Tin-Cam <mack679@hotmail.com.au>
2020-02-12 21:20:44 +1100

    Added basic code that makes the boss look at the player

---
+++ 5_Trials_2019/Assets/LookAtTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(Vector3.forward));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+    }
+}
