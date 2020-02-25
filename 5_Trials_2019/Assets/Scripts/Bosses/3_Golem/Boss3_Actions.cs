ce9b2feef729158b9983d6fffd0f161706352c55
Tin-Cam <mack679@hotmail.com.au>
2020-02-26 07:35:39 +1100

    Boss now animates when shooting a laser

--- 5_Trials_2019/Assets/Scripts/Bosses/3_Golem/Boss3_Actions.cs
+++ 5_Trials_2019/Assets/Scripts/Bosses/3_Golem/Boss3_Actions.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class BossAnimation
+{
+    //Animation Triggers
+    public static readonly string Idle = "Exit";
+
+    public static readonly string AttackStandard = "Atk_Standard";
+    public static readonly string AttackDesperation = "Atk_Desperation";
+    public static readonly string Fire = "Fire";
+}
+
 public class Boss3_Actions : _ActionBase
 {
     //public Laser laser;
@@ -9,16 +19,17 @@ public class Boss3_Actions : _ActionBase
     private Boss3_Controller controller;
     private LaserManager laserManager;
     private LookAtTarget lookAtTarget;
+    private Animator animator;
     private GameObject player;
 
     public float pushbackIntensity;
-    
 
     public void Init()
     {
         controller = GetComponent<Boss3_Controller>();
         laserManager = GetComponent<LaserManager>();
         lookAtTarget = GetComponentInChildren<LookAtTarget>();
+        animator = GetComponentInChildren<Animator>();
         player = controller.player;
 
         actionList.Add("Idle");
@@ -40,12 +51,17 @@ public class Boss3_Actions : _ActionBase
         lookAtTarget.isAiming = false;
         Quaternion angle = lookAtTarget.aimAngle;
 
+        //Indicate
+        animator.SetTrigger(BossAnimation.AttackStandard);
         yield return laserManager.IndicateLaser(1, angle);
-        //ShootLaser(player.transform.position);
-        //yield break;
-        //yield return new WaitForSeconds(2);
+        
+        //Fire
+        animator.SetTrigger(BossAnimation.Fire);
         yield return laserManager.ShootLaser(lookAtTarget.aimAngle);
+
+        //Back to Default State
         lookAtTarget.isAiming = true;
+        animator.SetTrigger(BossAnimation.Idle);
     }
 
     //Action 2 - Pushback player
