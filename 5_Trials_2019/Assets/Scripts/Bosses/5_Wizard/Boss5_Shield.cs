16700316bd18acf4d46f66f68cdfc5e45817c212
Tin-Cam <mack679@hotmail.com.au>
2020-06-22 20:04:10 +1000

    Started working on the shield

---
+++ 5_Trials_2019/Assets/Scripts/Bosses/5_Wizard/Boss5_Shield.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss5_Shield : MonoBehaviour
+{
+    public bool useShield;
+    public bool isShieldActive;
+    public bool isRecharging;
+
+    public SpriteRenderer shieldRender;
+
+    private int rechargeCounter;
+    private float shieldTimer;
+
+    public void Init()
+    {
+        ShieldActivated(false);
+    }
+
+    public void ShieldActivated(bool isActive)
+    {
+        isShieldActive = isActive;
+        shieldRender.enabled = isActive;
+
+        isRecharging = false;
+    }
+
+    public int IncrementRecharge()
+    {
+        if (!isRecharging)
+            return 0;
+
+        rechargeCounter++;
+        return rechargeCounter;
+    }
+}
