b59cb668197033a2fc53c009a114be1e1d716924
Tin-Cam <mack679@hotmail.com.au>
2020-09-28 22:05:07 +1000

    Started cutscene

---
+++ 5_Trials_2019/Assets/Scripts/Cutscenes/CM_Boss5.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CM_Boss5 : MonoBehaviour, ICutsceneManager
+{
+    public Camera camera;
+    private CameraFollow cameraFollow;
+
+    public Collider2D roomEntry;
+
+    public PlayableDirector introCutscene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Check if cutscene has already been played
+
+    }
+
+    public void PlayCutscene(){
+        Intro();
+    }
+
+    private void Intro(){
+        introCutscene.Play();
+    }
+
+    private void SkipIntro(){
+        //Initialise room, then start fight;
+        StartFight();
+    }
+
+    private void StartFight(){
+        //Start fight with boss
+    }
+
+    public void Ending(){
+
+    }
+}
---
+++ 5_Trials_2019/Assets/Scripts/Misc/CutsceneTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    public GameObject cutsceneManager;
+    private ICutsceneManager cutsceneManagerI;
+
+    void Start(){
+        cutsceneManagerI = cutsceneManager.GetComponent<ICutsceneManager>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player"){
+            cutsceneManagerI.PlayCutscene();
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
