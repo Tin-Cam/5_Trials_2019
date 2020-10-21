fc3bef426fb0357f103c79026350a42d2639407b
Tin-Cam <mack679@hotmail.com.au>
2020-10-21 20:48:51 +1100

    Glowy Eye!

---
+++ 5_Trials_2019/Assets/Scripts/Cutscenes/CM_Boss6.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CM_Boss6 : MonoBehaviour
+{
+    public PlayableDirector introCutscene;
+    public PlayableDirector endingCutscene;
+
+    public bool playCutscene;
+
+    public GameObject boss;
+    public GameObject cutSceneAssets;
+
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        if(playCutscene)
+            Intro();
+        else
+            SkipIntro();
+    }
+
+    private void Intro(){
+        //introCutscene.Play();
+        //StartFight();
+    }
+
+    private void SkipIntro(){
+        //Initialise room, then start fight;
+        cutSceneAssets.SetActive(false);
+        boss.SetActive(true);
+        StartFight();
+    }
+
+    private void StartFight(){
+        //Start fight with boss
+        gameManager.RoomIntro();
+    }
+
+    public void Ending(){
+
+    }
+}
