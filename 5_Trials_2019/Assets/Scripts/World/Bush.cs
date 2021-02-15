d51fb296a5f233a550dc7d0ef52b1d208c57f6e9
Tin-Cam <mack679@hotmail.com.au>
2021-02-15 21:52:13 +1100

    Bushes can now be cut

---
+++ 5_Trials_2019/Assets/Scripts/World/Bush.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+    public bool isCut = false;
+    public Sprite cutBush;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(isCut)
+            return;
+        if (other.tag == "Sword")
+            CutBush();
+    }
+
+    private void CutBush(){
+        Collider2D collider = GetComponent<Collider2D>();
+        collider.enabled = false;
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = cutBush;
+
+        isCut = true;
+    }
+
+}
