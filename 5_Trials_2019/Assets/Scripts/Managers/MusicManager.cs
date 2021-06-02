4c115f9abc54bda06925be4314771d5c287b9837
Tin-Cam <mack679@hotmail.com.au>
2021-06-02 20:36:07 +1000

    Created music manager

---
+++ 5_Trials_2019/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public bool hasIntro;
+
+    public AudioClip musicIntro;
+    public AudioClip musicLoop;
+
+    private AudioSource source;
+
+    void Awake(){
+        source = gameObject.AddComponent<AudioSource>();
+        source.loop = true;
+        source.clip = musicLoop;
+    }
+
+    void Start(){
+        PlayMusic();
+    }
+
+    public void PlayMusic(){
+        if(hasIntro || musicIntro == null)
+            PlayLoopWithIntro();
+        else
+            PlayOnlyLoop();
+    }
+
+    private void PlayLoopWithIntro(){
+        source.PlayOneShot(musicIntro);
+        source.PlayScheduled(AudioSettings.dspTime + musicIntro.length);
+    }
+
+    private void PlayOnlyLoop(){
+        source.Play();
+    }
+
+}
